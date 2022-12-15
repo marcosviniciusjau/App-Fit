b51bd1c1300d9c9576e5f1b44d8e2fa0b8070cfb
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-15 19:05:02 -0300

    Primeira Versão

---
+++ AppFit/AppFit/Models/Atividade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppFit.Models
+{
+   public class Atividade
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public double? Peso { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
